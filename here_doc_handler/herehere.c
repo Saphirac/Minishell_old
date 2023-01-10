a1782df8c633f92f09a17641e29101a0dae2077e
maparigi <maparigi@student.42.fr>
2023-01-10 01:20:07 +0100

    heredoc

--- here_doc_handler/herehere.c
+++ here_doc_handler/herehere.c
@@ -6,13 +6,15 @@
 /*   By: maparigi <maparigi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 14:32:38 by maparigi          #+#    #+#             */
-/*   Updated: 2023/01/09 15:36:53 by maparigi         ###   ########.fr       */
+/*   Updated: 2023/01/09 16:41:41 by maparigi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../includes/minishell.h"
 
-int	czeck_hd(const char *str)
+
+
+static int	czeck_hd(const char *str)
 {
 	const char	hdt[3] = "<<-";
 	const char	hd[2] = "<<";
@@ -32,9 +34,19 @@ void	find_hd(char *arg_l)
 	while (arg_l[++i])
 	{
 		if (czeck_hd(arg_l + i) == 2)
-			printf("nowdoc\n");
+			printf("heredoct : %s\n", arg_l + i);
 		else if (czeck_hd(arg_l + i) == 1)
-			printf("heredoc\n");
+			printf("heredoc : %s\n", arg_l + i);
 	}
 	return ;
 }
+
+/*******************************************/
+
+int	main(int ac, char **av)
+{
+	(void)ac;
+	find_hd(av[1]);
+}
+
+/*******************************************/
