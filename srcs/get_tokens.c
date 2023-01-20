074b10b3b51a3349593a450848ebf3a514ed2d76
Saphirac <mimiforspam1697@gmail.com>
2023-01-20 17:14:46 +0100

    Random behavior with eof on count token

--- srcs/get_tokens.c
+++ srcs/get_tokens.c
@@ -6,7 +6,7 @@
 /*   By: mcourtoi <mcourtoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 11:35:35 by mcourtoi          #+#    #+#             */
-/*   Updated: 2023/01/16 05:23:14 by mcourtoi         ###   ########.fr       */
+/*   Updated: 2023/01/20 17:09:23 by mcourtoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,12 @@ int	count_double_quotes(char *str, int i)
 			return (len_token + 1);
 		while (str[i] && str[i] != '"')
 		{
-			i++;
 			len_token++;
+			i++;
 			if (str[i] == '"')
-				return (len_token);
+				return (len_token + 1);
+			if (str[i] == '\0')
+				return (1);
 		}
 	}
 	return (len_token);
@@ -47,10 +49,12 @@ int	count_single_quotes(char *str, int i)
 			return (len_token + 1);
 		while (str[i] && str[i] != 39)
 		{
-			i++;
 			len_token++;
+			i++;
 			if (str[i] == 39)
-				return (len_token);
+				return (len_token + 1);
+			if (str[i] == '\0')
+				return (1);
 		}
 	}
 	return (len_token);
@@ -70,18 +74,30 @@ int	count_tokens(char *str)
 		if (str[i] == '\0')
 		{
 			if (str[i - 1] != ' ')
+			{
+				printf("here 1\n");
 				nb_tokens++;
+			}
 			break;
 		}
 		while (str[i] && str[i] != ' ' && str[i] != '"' && str[i] != 39)
 			i++;
 		if (str[i] == ' ' || str[i] == '\0')
+		{
+			printf("here 2\n");
 			nb_tokens++;
-		if (count_double_quotes(str, i) > 1)
+		}
+		if (count_double_quotes(str, i) > 0)
+		{
+			printf("here 3\n");
 			nb_tokens++;
+		}
 		i += count_double_quotes(str, i);
-		if (count_single_quotes(str, i) > 1)
+		if (count_single_quotes(str, i) > 0)
+		{
+			printf("here 4\n");
 			nb_tokens++;
+		}
 		i += count_single_quotes(str, i);
 	}
 	return (nb_tokens);
