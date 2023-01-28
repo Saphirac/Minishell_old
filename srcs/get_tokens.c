9acb2d7c1dbc326acabf53a8f968efbe76e1ee11
Saphirac <mimiforspam1697@gmail.com>
2023-01-28 18:57:20 +0100

    Bug corrected on count tokens

--- srcs/get_tokens.c
+++ srcs/get_tokens.c
@@ -6,7 +6,7 @@
 /*   By: mcourtoi <mcourtoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 11:35:35 by mcourtoi          #+#    #+#             */
-/*   Updated: 2023/01/20 17:09:23 by mcourtoi         ###   ########.fr       */
+/*   Updated: 2023/01/28 18:54:05 by mcourtoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,45 +60,42 @@ int	count_single_quotes(char *str, int i)
 	return (len_token);
 }
 
+// int	check_quotes(char **str, int *i)
+// {
+// 	int	count_single;
+// 	int	count_double;
+
+// 	count_single = count_double_quotes(*(str), *(i));		
+// 	count_double = count_single_quotes(*(str), *(i));
+// 	*(i) += count_double_quotes(*(str), *(i)), count_single_quotes(*(str), *(i));
+// 	if (count_single % 2 != 0 || count_double % 2 != 0)
+// 		return ((printf("nique ta mere la grosse tepu\n"), 1));
+// 	return (1);
+// }
+
 int	count_tokens(char *str)
 {
 	int	i;
-	int	nb_tokens;
+	int nb_tokens;
 
-	i = -1;
+	i = 0;
 	nb_tokens = 0;
-	while (str[++i])
+	while (str[i])
 	{
-		while (str[i] && str[i] == ' ')
+		while (str[i] && str[i] == 32)
 			i++;
-		if (str[i] == '\0')
-		{
-			if (str[i - 1] != ' ')
-			{
-				printf("here 1\n");
-				nb_tokens++;
-			}
-			break;
-		}
-		while (str[i] && str[i] != ' ' && str[i] != '"' && str[i] != 39)
+		if (!str[i])
+			return (nb_tokens);
+		while (str[i] != ' ' && str[i] != '"' && str[i] != '\''  && str[i])
 			i++;
-		if (str[i] == ' ' || str[i] == '\0')
-		{
-			printf("here 2\n");
+		if (!str[i])
+			return (nb_tokens + 1);
+		if (str[i] == ' ')
 			nb_tokens++;
-		}
 		if (count_double_quotes(str, i) > 0)
-		{
-			printf("here 3\n");
-			nb_tokens++;
-		}
-		i += count_double_quotes(str, i);
+			(nb_tokens++, i += count_double_quotes(str, i));
 		if (count_single_quotes(str, i) > 0)
-		{
-			printf("here 4\n");
-			nb_tokens++;
-		}
-		i += count_single_quotes(str, i);
+			(nb_tokens++, i += count_single_quotes(str, i));
 	}
 	return (nb_tokens);
 }
